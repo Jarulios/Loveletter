8d7868dca7708b351d4139a079314629ad9e2384
Jarlo <mrjarulios@gmail.com>
2021-11-25 16:34:12 +1300

    switched to object-oriented variables, fixed errors so script can now run without code breaking

--- data_gen.py
+++ data_gen.py
@@ -1,14 +1,43 @@
 
+class Data(object):
+
+    def __init__(self, hand, deck, vision, opponent_vision, opponent_hand, handmaid_flag, focus_person,
+                 max_depth=6):
+        self.hand = hand
+        self.deck = deck
+        self.vision = vision
+        self.opponent_vision = opponent_vision
+        self.opponent_hand = opponent_hand
+        self.handmaid_flag = handmaid_flag
+        self.depth = 0
+        self.max_depth = max_depth
+        self.focus_person = focus_person
+
+    def switch(self):
+        self.hand, \
+            self.vision, \
+            self.opponent_hand, \
+            self.opponent_vision = \
+            self.opponent_hand, \
+            self.opponent_vision, \
+            self.hand, \
+            self.vision
+        if self.focus_person == 'player':
+            self.focus_person = 'opponent'
+        else:
+            self.focus_person = 'player'
+
+
 def countess_check(hand):
     flag1, flag2 = False, False
     for i in hand:
         if i == 5:
             flag2 = True
-        elif i == 7:
-            flag1 = True
-        elif i == 8:
-            flag2 = True
         elif i == 6:
+            flag1 = True
+        elif i == 7:
+            flag2 = True
+        elif i == 8:
             flag2 = True
         else:
             return False
@@ -30,229 +59,166 @@ def guard_check(hand, vision):
         return False
 
 
-def turn(hand, deck, vision, opponent_vision, opponent_hand, handmaid_flag, turn_win_count, turn_loss_count,
-         turn_draw_count, current_depth, max_depth=6):
-    print('new turn')
-    if not depth > max_depth:
+def turn(info):
+    win_count = 0
+    loss_count = 0
+    draw_count = 0
+    if not info.depth > info.max_depth:
+        info.depth += 1
         if not len(current_deck) == 0:
 
-            temp_deck = deck
-            for card in range(len(deck)):
-                hand.append(temp_deck.pop(card))
+            info.switch()
+            for card in range(len(info.deck)):
+                info.hand.append(info.deck[card])
 
                 decision_flag = False
-
-                if not handmaid_flag:
+                if not info.handmaid_flag:
                     # checks for hard rules
-                    if not vision == 0:
-                        if guard_check(hand, vision):
-                            if hand[0] == 1:
-                                del hand[0]
+                    if not info.vision == 0:
+                        if guard_check(info.hand, info.vision):
+                            if info.hand[0] == 1:
+                                del info.hand[0]
                             else:
-                                del hand[1]
+                                del info.hand[1]
                             decision_flag = True
-                            turn_win_count += 1
+                            win_count += 1
 
-                        elif vision == 8:
-                            if hand[0] == 5 or hand[1] == 5:
-                                if hand[0] == 5:
-                                    del hand[0]
+                        elif info.vision == 8:
+                            if info.hand[0] == 5 or info.hand[1] == 5:
+                                if info.hand[0] == 5:
+                                    del info.hand[0]
                                 else:
-                                    del hand[1]
+                                    del info.hand[1]
                                 decision_flag = True
-                                turn_win_count += 1
+                                win_count += 1
 
-                        elif hand[0] == 3:
-                            if hand[1] > vision:
-                                del hand[0]
+                        elif info.hand[0] == 3:
+                            if info.hand[1] > info.vision:
+                                del info.hand[0]
                                 decision_flag = True
-                                turn_win_count += 1
-                        elif hand[1] == 3:
-                            if hand[0] > vision:
-                                del hand[1]
+                                win_count += 1
+                        elif info.hand[1] == 3:
+                            if info.hand[0] > info.vision:
+                                del info.hand[1]
                                 decision_flag = True
-                                turn_win_count += 1
+                                win_count += 1
 
-                elif countess_check(hand):
-                    if hand[0] == 7:
-                        del hand[0]
+                elif countess_check(info.hand):
+                    if info.hand[0] == 7:
+                        del info.hand[0]
                     else:
-                        del hand[1]
+                        del info.hand[1]
                     decision_flag = True
 
                 # plays a card
                 if not decision_flag:
                     for x in (0, 1):
-                        temp_hand = hand
-                        print(hand)
-                        print(x)
+                        temp_hand = info.hand
                         decision = temp_hand.pop(x)
-                        if handmaid_flag:
-                            handmaid_flag = False
-                            print('handmaid')
-                            print(decision)
+                        if info.handmaid_flag:
+                            info.handmaid_flag = False
 
                             if decision == 4:
-                                handmaid_flag = True
+                                info.handmaid_flag = True
 
                             elif decision == 5:
                                 for choice in ['self', 'opponent']:
                                     if choice == 'self':
                                         if temp_hand[0] == 8:
-                                            turn_loss_count += 1
+                                            loss_count += 1
                                         else:
-                                            for new_card in temp_deck:
-                                                temp_hand = [new_card]
-                                                opponent_vision = 0
-                                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, \
-                                                    turn_loss_count = opponent_hand, opponent_vision, turn_loss_count, \
-                                                    temp_hand, vision, turn_win_count
-                                                current_depth += 1
-                                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand,
-                                                     turn_win_count, turn_loss_count, turn_draw_count, depth, max_depth)
-                                                current_depth -= 1
-                                    temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count \
-                                        = opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, \
-                                        turn_win_count
-                                    current_depth += 1
-                                    turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                         turn_loss_count, turn_draw_count, depth, max_depth)
-                                    current_depth -= 1
+                                            info.opponent_vision = 0
+                                            for new_card in info.deck:
+                                                info.hand = [new_card]
+                                                win_count, loss_count, draw_count = turn(info)
+                                    else:
+                                        win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 8:
-                                turn_loss_count += 1
+                                loss_count += 1
 
-                            temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count = \
-                                opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, turn_win_count
-                            current_depth += 1
-                            turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                 turn_loss_count, turn_draw_count, depth, max_depth)
-                            current_depth -= 1
+                            win_count, loss_count, draw_count = turn(info)
 
                         else:
                             if decision == 1:
                                 for guess in [2, 3, 4, 5, 6, 7, 8]:
-                                    if guess == opponent_hand:
-                                        turn_win_count += 1
+                                    if guess == info.opponent_hand:
+                                        win_count += 1
                                     else:
-                                        temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, \
-                                            turn_loss_count = opponent_hand, opponent_vision, turn_loss_count, \
-                                            temp_hand, vision, turn_win_count
-                                        current_depth += 1
-                                        turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                             turn_loss_count,
-                                             turn_draw_count, depth, max_depth)
-                                        current_depth -= 1
+                                        win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 2:
-                                vision = opponent_hand
-                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count = \
-                                    opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, turn_win_count
-                                current_depth += 1
-                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                     turn_loss_count,
-                                     turn_draw_count, depth, max_depth)
-                                current_depth -= 1
+                                info.vision = info.opponent_hand
+                                win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 3:
-                                if temp_hand > opponent_hand:
-                                    turn_win_count += 1
-                                elif temp_hand < opponent_hand:
-                                    turn_loss_count += 1
+                                if temp_hand > info.opponent_hand:
+                                    win_count += 1
+                                elif temp_hand < info.opponent_hand:
+                                    loss_count += 1
                                 else:
-                                    vision = opponent_hand
-                                    opponent_vision = temp_hand
-                                    temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count \
-                                        = opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, \
-                                        turn_win_count
-                                    current_depth += 1
-                                    turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                         turn_loss_count, turn_draw_count, depth, max_depth)
-                                    current_depth -= 1
+                                    info.vision = info.opponent_hand
+                                    info.opponent_vision = temp_hand
+                                    win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 4:
-                                handmaid_flag = True
-                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count = \
-                                    opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, turn_win_count
-                                current_depth += 1
-                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                     turn_loss_count, turn_draw_count, depth, max_depth)
-                                current_depth -= 1
+                                info.handmaid_flag = True
+                                win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 5:
                                 for choice in ['self', 'opponent']:
                                     if choice == 'self':
                                         if temp_hand[0] == 8:
-                                            turn_loss_count += 1
+                                            loss_count += 1
                                         else:
-                                            for new_card in temp_deck:
+                                            for new_card in info.deck:
                                                 temp_hand = new_card
-                                                opponent_vision = 0
-                                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, \
-                                                    turn_loss_count = opponent_hand, opponent_vision, turn_loss_count, \
-                                                    temp_hand, vision, turn_win_count
-                                                current_depth += 1
-                                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand,
-                                                     turn_win_count, turn_loss_count, turn_draw_count, depth, max_depth)
-                                                current_depth -= 1
+                                                info.opponent_vision = 0
+                                                win_count, loss_count, draw_count = turn(info)
                                     else:
-                                        if opponent_hand[0] == 8:
-                                            turn_win_count += 1
+                                        if info.opponent_hand[0] == 8:
+                                            win_count += 1
                                         else:
-                                            for new_card in temp_deck:
-                                                opponent_hand = new_card
-                                                vision = 0
-                                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, \
-                                                    turn_loss_count = opponent_hand, opponent_vision, turn_loss_count, \
-                                                    temp_hand, vision, turn_win_count
-                                                current_depth += 1
-                                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand,
-                                                     turn_win_count, turn_loss_count, turn_draw_count, depth, max_depth)
-                                                current_depth -= 1
+                                            for new_card in info.deck:
+                                                info.opponent_hand = new_card
+                                                info.vision = 0
+                                                win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 6:
-                                temp_hand, opponent_hand = opponent_hand, temp_hand
-                                vision = enemy_hand
-                                opponent_vision = temp_hand
-                                temp_hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count = \
-                                    opponent_hand, opponent_vision, turn_loss_count, temp_hand, vision, turn_win_count
-                                current_depth += 1
-                                turn(temp_hand, deck, vision, opponent_vision, opponent_hand, turn_win_count,
-                                     turn_loss_count, turn_draw_count, depth, max_depth)
-                                current_depth -= 1
+                                temp_hand, opponent_hand = info.opponent_hand, temp_hand
+                                info.vision = enemy_hand
+                                info.opponent_vision = temp_hand
+                                win_count, loss_count, draw_count = turn(info)
 
                             elif decision == 8:
-                                turn_loss_count += 1
+                                loss_count += 1
                         # end of play script
                     # end of for
                 else:
-                    hand, vision, turn_win_count, opponent_hand, opponent_vision, turn_loss_count = \
-                        opponent_hand, opponent_vision, turn_loss_count, hand, vision, turn_win_count
-                    current_depth += 1
-                    turn(hand, deck, vision, opponent_vision, opponent_hand, turn_win_count, turn_loss_count,
-                         turn_draw_count, depth, max_depth)
-                    current_depth -= 1
+                    win_count, loss_count, draw_count = turn(info)
+            if info.hand > info.opponent_hand:
+                win_count += 1
+            elif info.hand > info.opponent_hand:
+                loss_count += 1
+            else:
+                draw_count += 1
 
-    return turn_win_count, turn_loss_count
+    info.depth -= 1
+    return win_count, loss_count, draw_count
 
 
 cards = [1, 2, 3, 4, 5, 6, 7, 8]
 current_deck = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
 player_hand = []
-player_vision = 0
 player_guard_flag = False
 player_countess_drop_flag = False
 player_countess_flag = False
 enemy_hand = []
-enemy_vision = 0
 enemy_guard_flag = False
 enemy_countess_drop_flag = False
 enemy_countess_flag = False
-win_count = 0
-loss_count = 0
-draw_count = 0
 
-depth = 3
+max_run_depth = 4
 
 
 # deals first hand
@@ -261,4 +227,6 @@ for player_card in current_deck:
     for enemy_card in current_deck:
         enemy_hand.append(current_deck.pop(enemy_card))
 
-        print(turn(player_hand, current_deck, player_vision, enemy_vision, enemy_hand, False, 0, 0, 0, 0, 4))
+        setup_data = Data(player_hand, current_deck, 0, 0, enemy_hand, False, 'player', max_run_depth)
+        print(5*'-'+'new game'+5*'-')
+        print(turn(setup_data))
